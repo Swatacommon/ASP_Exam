0ecce7150298c67ce2ac55de6692a1e431fde677
Swatar Common <swatarcommon@gmail.com>
2020-07-01 14:20:24 +0300

    Complete Task 9,10

---
+++ 10.ASP.NET ASMX,WSDL,SOAP, прокси,порядокразработки/Task10/TestASMX/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestASMX.AsmxService;
+
+namespace TestASMX
+{
+    class Program
+    {
+        private static WebService1SoapClient Service { get; set; }
+        static void Main(string[] args)
+        {
+            Service = new WebService1SoapClient();
+            Console.WriteLine(Service.HelloWorld());
+            Console.WriteLine(Service.Sum(1,7));
+        }
+    }
+}
---
+++ 9._ASP.NEThttp_обработчикиhttpобработчик/Task9/Task9/WebSocketHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace Task9
+{
+    public class WebSocketHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+        private WebSocket webSocket;
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.HttpMethod == "GET" && context.IsWebSocketRequest != true)
+            {
+                string textFromFile = File.ReadAllText(HttpContext.Current.Server.MapPath("~/html.html"));
+                context.Response.ContentType = "text/html";
+                context.Response.Write(textFromFile);
+            }
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketRequest);
+            }
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context)
+        {
+            webSocket = context.WebSocket;
+            while (true)
+            {
+                var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(DateTime.Now.ToLongDateString()));
+                Thread.Sleep(1500);
+                try
+                {
+                    if (webSocket.State == WebSocketState.Open)
+                    {
+                        await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                    }
+                }
+                catch (ObjectDisposedException)
+                {
+                    webSocket.Abort();
+                }
+            }
+        }
+    }
+}
