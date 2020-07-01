8b45f5c6127c9d9925c54103adcc3f14c58bd88e
Swatar Common <swatarcommon@gmail.com>
2020-07-01 17:53:52 +0300

    Complete Task 19

---
+++ 19._WCF-сервисы WSDL,хост,прокси,моде/Task19/TestWCF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWCF.WCFService;
+
+namespace TestWCF
+{
+    class Program
+    {
+        private static IService1 Service { get; set; }
+        static void Main(string[] args)
+        {
+            Service = new Service1Client();
+            var phones = Service.GetDict();
+            foreach(var item in phones)
+            {
+                Console.WriteLine($"Имя {item.Name} | Номер Телефона {item.Phone_Number}");
+            }
+        }
+    }
+}
