30045a3aa9def3cfa7fdf281eeffeaf546038f73
Swatar Common <swatarcommon@gmail.com>
2020-07-01 12:01:38 +0300

    Complete Task 4

---
+++ 4._Протокол_WebSockets_принципы_работы_и_применения/Task4/Task4/WebSocketHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace Task4
+{
+    public class WebSocketHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+        private WebSocket webSocket;
+        public void ProcessRequest(HttpContext context)
+        {
+            if(context.Request.HttpMethod == "GET" && context.IsWebSocketRequest != true)
+            {
+                string textFromFile = File.ReadAllText(HttpContext.Current.Server.MapPath("~/getpost.html"));
+                context.Response.ContentType = "text/html";
+                context.Response.Write(textFromFile);
+            }
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketRequest);
+            }
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context)
+        {
+            webSocket = context.WebSocket;
+            while (true)
+            {
+                var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(DateTime.Now.ToLongDateString()));
+                Thread.Sleep(2500);
+                try
+                {
+                    if (webSocket.State == WebSocketState.Open)
+                    {
+                        await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                    }
+                }
+                catch (ObjectDisposedException)
+                {
+                    webSocket.Abort();
+                }
+            }
+        }
+    }
+}
